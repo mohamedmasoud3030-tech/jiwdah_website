ad8381bf43d74aa354372f2bbc10ffd2a5bc47c6
mohamedmasoud3030-tech <250202280+mohamedmasoud3030-tech@users.noreply.github.com>
2026-04-29 09:21:33 -0400

    Fix: Add TRPCProvider and vercel.json for deployment

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import App from './App'
+import { TRPCProvider } from './providers/trpc'
 import './App.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <TRPCProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </TRPCProvider>
   </StrictMode>,
 )
